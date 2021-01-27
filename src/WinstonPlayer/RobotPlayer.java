cb1b07d3e64a805634be4d0d6158f6b4a9788836
Ben Underwood <benu423@gmail.com>
2021-01-27 14:48:47 -0500

    Primitive Change
    Using a primitive objectively saves memory compared to using an Object

--- src/WinstonPlayer/RobotPlayer.java
+++ src/WinstonPlayer/RobotPlayer.java
@@ -1047,7 +1047,7 @@ public strictfp class RobotPlayer {
 
 	static void smartBid() throws GameActionException {
 		int currentVotes = rc.getTeamVotes();
-		Boolean voteGained = currentVotes > lastRoundVotes;
+		boolean voteGained = currentVotes > lastRoundVotes;
 		if (rc.canBid(lastRoundBid) && voteGained) {
 			if (roundsPlateaued == 15) {
 				lastRoundBid /= 2;
